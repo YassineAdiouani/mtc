672ff60df3f01b70835dfa97f88deedeb93c756a
elijaah <belahcenilyasse6@gmail.com>
2023-11-28 12:12:27 +0100

    update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,7 +7,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-cursus</title>
+    <title>MTC</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/master.css">
     <header class="cont">        
 		<nav style="box-shadow: 0 2px 10px rgba(0,0,0,.3) ;
@@ -32,6 +33,12 @@
 						@if (Route::has('login'))
                         @auth
                             <li class="nav-item me-4"><a class="nav-link" href="{{ url('/home') }}" class="button">Mon compte</a></li>
+							<li class="nav-item me-4">
+								<form method="POST" action="{{ route('logout') }}">
+									@csrf
+									<button class="btn btn-secondary " type="submit">Log Out</button>
+								</form>
+							</li>
                         @else
                             <li class="nav-item me-4"><a class="nav-link" href="{{ route('login') }}" class="button">Log in</a>
                             @if (Route::has('register'))
@@ -44,13 +51,16 @@
 			</div>
 		</nav>
     </header>
-
-
+	
+	
 </head>
 <body>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@2.11.6/dist/umd/popper.min.js" integrity="sha384-ZScJkPQjI6gXFahFtEUsRxHA00/efMivYKtRq4ii+60VSNl/6I6eCvk1xE9R+X3S" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <div  style="margin-top:40px;" class="body">
         @yield('contenu')
     </div>
+
 </body>
 </html>
 
