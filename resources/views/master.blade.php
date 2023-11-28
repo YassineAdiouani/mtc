e0d469147581b1b7e43d164b56602f9a271318b4
yassinediwani <yassinediwani444@gmail.com>
2023-11-28 12:10:11 +0100

    update accueil design

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,8 +7,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-cursus</title>
-    <link rel="stylesheet" href="/css/master.css">
+	<link rel="stylesheet" href="/css/master.css">
+    <title>E-cursus - @yield('title')</title>
     <header class="cont">        
 		<nav style="box-shadow: 0 2px 10px rgba(0,0,0,.3) ;
 		background-color:whitesmoke;
